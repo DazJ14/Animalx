a08c93c82b5edc595a42fdbeb120cab05aed2e2a
Diego Zamora <jaguares_diego@hotmail.com>
2025-04-25 22:09:52 -0600

    refactor: update App component structure and remove unnecessary routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,62 @@
-import "./App.css";
-import BodyLayout from "./components/body/BodyLayout";
-import FooterLayout from "./components/footer/FooterLayout";
-import NavigationLayout from "./components/navigation/NavigationLayout";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/body/HomeLayout/Home";
-import About from "./components/body/About";
-import Services from "./components/body/Services";
+import FooterLayout from "./components/sections/FooterLayout";
+import background from "./assets/noise-bg.png"
 import { useState, useEffect } from "react";
+import DesktopFloatingNavigation from "./components/navigation/DesktopFloatingNavigation";
+import MobileNavigation from "./components/navigation/MobileNavigation";
+import { Route, Switch } from "wouter";
+import { ROUTES } from "./routes";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import { useWindowScroll, useWindowSize } from "@uidotdev/usehooks";
+import DesktopNavigation from "./components/navigation/DesktopNavigation";
 
 function App() {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [{ y: scrollY }] = useWindowScroll()
+  const { width: clientWindowWidth } = useWindowSize()
+  const [isMobile, setIsMobile] = useState(clientWindowWidth < 1024);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [floatingNavigationDown, setFloatingNavigationDown] = useState(false);                                     
 
   useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 0);
-    };
+    setIsMobile(clientWindowWidth < 1024)
+  }, [clientWindowWidth])
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  useEffect(() => {
+    if(!isMobile && isMobileMenuOpen) {
+      setIsMobileMenuOpen(false)
+    }
+  }, [isMobile, isMobileMenuOpen])
+
+  useEffect(() => {
+    const htmlTag = document.documentElement.style
+    htmlTag.overflow = isMobileMenuOpen ? "hidden" : ""
+  }, [isMobileMenuOpen])
+
+  useEffect(() => {
+    setFloatingNavigationDown(scrollY > 1060)
+  }, [scrollY])
 
   return (
-    <>
-      <NavigationLayout isScrolled={isScrolled} />
-      <BodyLayout>
-        <Routes>
-          <Route path="/Animalx" element={<Home />} />
-          <Route path="/Animalx/about" element={<About />} />
-          <Route path="/Animalx/services" element={<Services />} />
-        </Routes>
-      </BodyLayout>
-      <FooterLayout />
-    </>
+    <div id="app" className="w-full h-full relative">
+
+      { isMobile && <MobileNavigation toggleMenu={() => setIsMobileMenuOpen(isOpen => !isOpen)} isMenuOpen={isMobileMenuOpen}/>}
+      
+      <div className="bg-[rgb(35,35,35)] xl:p-5 pb-0 w-full h-auto relative">
+        <DesktopNavigation />
+        { !isMobile && <DesktopFloatingNavigation navigationActive={floatingNavigationDown}/> }
+        <main className="min-h-[calc(100vh-20px)] bg-[hsl(44,52%,96%)] rounded-b-[32px] xl:rounded-t-[32px] relative z-20 overflow-hidden pb-20">
+          <Switch>
+            <Route path={ROUTES.home.path} component={HomePage} />
+            <Route path={ROUTES.blog.path} component={AboutPage} />
+            {/* <Route path={ROUTES.placeholder.path} component={HomePage} />
+            <Route path={ROUTES.placeholder.path} component={HomePage} /> */}
+            <Route>404, Not Found!</Route> 
+          </Switch>
+        </main>
+        <FooterLayout />
+        <div className="w-full h-full absolute top-0 left-0 bg-auto bg-left-top bg-repeat opacity-15" style={{ backgroundImage: `url('${background}')`}} />
+      </div>
+    </div>
   );
 }
 
