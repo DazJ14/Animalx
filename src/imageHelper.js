4b2c36753a1da09d7e3641f129a8d2b61ad1a043
Diego Zamora <jaguares_diego@hotmail.com>
2025-04-25 22:10:46 -0600

    feat: add image helper and routing configuration for the application

---
+++ src/imageHelper.js
@@ -0,0 +1,20 @@
+import dog_1 from "./assets/cute-dog-comprimido.png"
+import dog_2 from "./assets/golden-retriever.png"
+import cat_1 from "./assets/feral-cat.png"
+
+export const IMAGES = {
+    heroImages: [
+        {
+            image: dog_1,
+            name: "dog_1"
+        },
+        {
+            image: dog_2,
+            name: "dog_2"
+        },
+        {
+            image: cat_1,
+            name: "cat_1"
+        }
+    ]
+}
---
+++ src/routes.js
@@ -0,0 +1,31 @@
+export const ROUTES = {
+  home: {
+    path: "/",
+    title: "Inicio"
+  },
+  sobreAnimalx: {
+    title: "Animalx",
+    routes: [
+      {
+        path: "animalx",
+        title: "Sobre Animalx"
+      },
+      {
+        path: "servicios",
+        title: "Servicios"
+      },
+      {
+        path: "contacto",
+        title: "Contactanos"
+      }
+    ]
+  },
+  blog: {
+    path: "/blog",
+    title: "Blog"
+  },
+  placeholder: {
+    path: "/",
+    title: "Placeholder"
+  }
+}
